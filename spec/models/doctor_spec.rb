21723d90f649aaa94fd47e9637a34688d3929532
Kyle Boss <kyle@kyleboss.com>
2018-01-30 17:16:33 -0800

    Updated specs to for the ability to return 0 if there are no comments for a doc

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,9 +3,18 @@
 describe Doctor do
   describe '#recent_average_rating' do
     let(:doctor) { FactoryBot.build_stubbed(:doctor) }
-    before(:each) { expect(Comment).to receive(:recent_average_rating_for).with(doctor) { 3 } }
-    it 'returns what Comment.recent_average_rating_for returns' do
-      expect(doctor.recent_average_rating).to eq 3
+    before(:each) { allow(Comment).to receive(:recent_average_rating_for).with(doctor) { 3 } }
+    context 'doctor has no comments' do
+      it 'returns 0' do
+        expect(doctor.recent_average_rating).to eq 0
+      end
+    end
+
+    context 'doctor has comments' do
+      let(:doctor) { FactoryBot.build_stubbed(:doctor, comments: [FactoryBot.build_stubbed(:comment)]) }
+      it 'returns what Comment.recent_average_rating_for returns' do
+        expect(doctor.recent_average_rating).to eq 3
+      end
     end
   end
 end
